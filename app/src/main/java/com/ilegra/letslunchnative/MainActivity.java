a72f86549ae326fa9903a1ea88827e4b2e7f403c
Renan Rhoden <renanrhoden@gmail.com>
2017-06-29 18:48:35 -0300

    add like and dislike buttons

--- app/src/main/java/com/ilegra/letslunchnative/MainActivity.java
+++ app/src/main/java/com/ilegra/letslunchnative/MainActivity.java
@@ -4,12 +4,15 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,13 +28,18 @@ public class MainActivity extends AppCompatActivity {
         TextView header = (TextView) findViewById(R.id.toolbarHeaderId);
         final TextView restaurantName = (TextView) findViewById(R.id.restaurantNameId);
         final TextView restaurantAddress = (TextView) findViewById(R.id.restaurantAddressId);
+        final ImageView restaurantImage = (ImageView) findViewById(R.id.restaurantImageId);
         Typeface font = Typeface.createFromAsset(getAssets(), "font/Bitter-Bold.ttf");
         header.setTypeface(font);
         restaurantName.setTypeface(font);
+        // Create global configuration and initialize ImageLoader with this config
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+            .build();
+        ImageLoader.getInstance().init(config);
 
         String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
         final JSONArray[] result = new JSONArray[1];
-        String urlImage = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms"
+        final String urlImage = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
 
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -40,10 +48,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         //Log.i("Response", response.toString());
                         try {
+                            ImageLoader imageLoader = ImageLoader.getInstance();
                             result[0] = response.getJSONArray("results");
                             Log.i("Response", String.valueOf(result[0].length()));
                             restaurantName.setText(result[0].getJSONObject(0).getString("name"));
                             restaurantAddress.setText(result[0].getJSONObject(0).getString("vicinity"));
+                            imageLoader.displayImage(urlImage, restaurantImage);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
