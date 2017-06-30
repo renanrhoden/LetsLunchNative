2257bcf007c9b62567beb1ddf2dd17a011573433
Renan Rhoden <renanrhoden@gmail.com>
2017-06-30 15:03:09 -0300

    image going forward after clicking the like button

--- app/src/main/java/com/ilegra/letslunchnative/MainActivity.java
+++ app/src/main/java/com/ilegra/letslunchnative/MainActivity.java
@@ -2,23 +2,18 @@ package com.ilegra.letslunchnative;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.ContextWrapper;
-import android.graphics.Bitmap;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -26,15 +21,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
     private Activity mActivity;
+    private String urlImage;
+    private JSONArray[] result;
+    private TextView restaurantName;
+    private TextView restaurantAddress;
+    private ImageView restaurantImage;
+    ImageLoader imageLoader;
+    private int restaurantPosition = 0;
+
+    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,34 +45,37 @@ public class MainActivity extends AppCompatActivity {
         mActivity = MainActivity.this;
 
         TextView header = (TextView) findViewById(R.id.toolbarHeaderId);
-        final TextView restaurantName = (TextView) findViewById(R.id.restaurantNameId);
-        final TextView restaurantAddress = (TextView) findViewById(R.id.restaurantAddressId);
-        final ImageView restaurantImage = (ImageView) findViewById(R.id.restaurantImageId);
+        restaurantName = (TextView) findViewById(R.id.restaurantNameId);
+        restaurantAddress = (TextView) findViewById(R.id.restaurantAddressId);
+        restaurantImage = (ImageView) findViewById(R.id.restaurantImageId);
+
         Typeface font = Typeface.createFromAsset(getAssets(), "font/Bitter-Bold.ttf");
         header.setTypeface(font);
         restaurantName.setTypeface(font);
+
         // Create global configuration and initialize ImageLoader with this config
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
                 .build();
         ImageLoader.getInstance().init(config);
 
-        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
-        final JSONArray[] result = new JSONArray[1];
-        final String urlImage = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
+        result = new JSONArray[1];
 
+        getFirstData();
+
+
+    }
+
+    private void getFirstData() {
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        //Log.i("Response", response.toString());
                         try {
-                            ImageLoader imageLoader = ImageLoader.getInstance();
+                            imageLoader = ImageLoader.getInstance();
                             result[0] = response.getJSONArray("results");
-                            Log.i("Response", String.valueOf(result[0].length()));
-                            restaurantName.setText(result[0].getJSONObject(0).getString("name"));
-                            restaurantAddress.setText(result[0].getJSONObject(0).getString("vicinity"));
-                            imageLoader.displayImage(urlImage, restaurantImage);
+                            updateRestaurantInfo();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -89,79 +91,35 @@ public class MainActivity extends AppCompatActivity {
 
 // Access the RequestQueue through your singleton class.
         MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
-
-        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
-
-        // Initialize a new ImageRequest
-        ImageRequest imageRequest = new ImageRequest(
-                urlImage, // Image URL
-                new Response.Listener<Bitmap>() { // Bitmap listener
-                    @Override
-                    public void onResponse(Bitmap response) {
-                        // Do something with response
-                        //restaurantImage.setImageBitmap(response);
-                        // Save this downloaded bitmap to internal storage
-                        Uri uri = saveImageToInternalStorage(response);
-
-                        // Display the internal storage saved image to image view
-                        //restaurantImage.setImageURI(uri);
-                    }
-                },
-                0, // Image width
-                0, // Image height
-                ImageView.ScaleType.CENTER_CROP, // Image scale type
-                Bitmap.Config.RGB_565, //Image decode configuration
-                new Response.ErrorListener() { // Error listener
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Do something with error response
-                        error.printStackTrace();
-                    }
-                }
-        );
-
-// Add ImageRequest to the RequestQueue
-        requestQueue.add(imageRequest);
-
     }
 
-    protected Uri saveImageToInternalStorage(Bitmap bitmap){
-        // Initialize ContextWrapper
-        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+    private void updateRestaurantInfo() throws JSONException {
+        restaurantName.setText(result[0].getJSONObject(restaurantPosition).getString("name"));
+        restaurantAddress.setText(result[0].getJSONObject(restaurantPosition).getString("vicinity"));
+        JSONArray photo = result[0].getJSONObject(restaurantPosition).getJSONArray("photos");
+        imageLoader.displayImage(getUrlForImage(getPhotoReference(photo, "photo_reference")), restaurantImage);
+        restaurantPosition++;
+    }
 
-        // Initializing a new file
-        // The bellow line return a directory in internal storage
-        File file = wrapper.getDir("Images",MODE_PRIVATE);
+    private String getPhotoReference(JSONArray photo, String photo_reference) throws JSONException {
+        return photo.getJSONObject(0).getString(photo_reference);
+    }
 
-        // Create a file to save the image
-        file = new File(file, "UniqueFileName"+".jpg");
+    private String getUrlForImage(String reference){
+        String url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
+        String myKey = "&key=AIzaSyCp5OnViFVhTyj9R-3RuyWgyCaAGlbxdms";
 
-        try{
-            // Initialize a new OutputStream
-            OutputStream stream = null;
 
-            // If the output file exists, it can be replaced or appended to it
-            stream = new FileOutputStream(file);
+        return url + reference + myKey;
+    }
 
-            // Compress the bitmap
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
-
-            // Flushes the stream
-            stream.flush();
-
-            // Closes the stream
-            stream.close();
-
-        }catch (IOException e) // Catch the exception
-        {
+    public void onLikeClick(View view){
+        try {
+            updateRestaurantInfo();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        // Parse the gallery image url to uri
-        Uri savedImageURI = Uri.parse(file.getAbsolutePath());
-
-        // Return the saved image Uri
-        return savedImageURI;
     }
+
 }
 
